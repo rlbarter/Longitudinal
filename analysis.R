ba934943148a6bbe007ba2bdf891891ca0e2cc0c
stephaniesookim <stephaniekim@Stephanies-MBP.att.net>
2015-11-19 02:25:44 -0800

    kml method tested

--- analysis.R
+++ analysis.R
@@ -37,15 +37,17 @@ meps_second <- meps_shifted %>%
   mutate(period3 = ER_shifted_expense[3])
 
 meps_new <- meps_second
+save(meps_new, file="MEPS_new.RData")
+
 
 # look at the shifted trajectories
 ggplot(filter(meps_new, id %in% index)) + geom_line(aes(x = as.numeric(period), y = ER_shifted_expense, col = id))
 
 ##################### Clustering #####################
-index <- sample(unique(meps_clean$id), 1000)
+index <- sample(unique(meps_clean$id),500)
 
 ### K-means-clustering
-kmeans <- kmeans(dplyr::select(meps_new,ER_sd_expense,ER_shifted_mean), centers = 10)
+kmeans <- kmeans(dplyr::select(meps_new,ER_sd_expense,ER_shifted_mean), centers = 5)
 # plot the clusters
 meps_new_clustered <- meps_new
 meps_new_clustered$cluster <- factor(kmeans$cluster)
@@ -60,4 +62,33 @@ meps_new_clustered2$cluster <- factor(clara$clustering)
 ggplot(filter(meps_new_clustered2, id %in% index)) + geom_line(aes(x = as.numeric(period), y = ER_shifted_expense, group=id, col = cluster))
 ggplot(meps_new_clustered2) + geom_line(aes(x = as.numeric(period), y = ER_shifted_expense, group = id, col = cluster))
 
+################ KML (K-means for Longitudinal Data) #############
+library(kml3d)
+library(kml)
+library(reshape)
+
+### let's just consider ER_shifted_expense only for testing kml method
+new <- as.data.frame(meps_new)
+new2 <- as.matrix(new$ER_shifted_expense)
+id <- unique(new$id)
+period <- unique(new$period)
+
+### changed into wide format
+wide <- matrix(NA,nrow=length(id),ncol=length(period))
+colnames(wide) <- period
+wide[,1] = id
+
+for(i in 1:length(id)){
+  for(j in 1:length(period)){
+    wide[i,j] = new2[5*i+j-5]
+  }
+}
+
+View(wide)
+
+cld = clusterLongData(wide,idAll = id)
+kml(cld,nbClusters=5,toPlot='both')
+
 # haven't figured out how to cluster data cleary enough..
+
+
